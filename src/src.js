a71065c0bfdbb5e1f7ef51c26d8c37fd8086f573
a.zagoveliev <63207977+mexell96@users.noreply.github.com>
2020-06-18 15:54:35 +0300

    Update src.js

--- src/src.js
+++ src/src.js
@@ -1,86 +1,3 @@
-const currIds = [145, 292, 298];
-const currenciesCache = {};
-
-let currRate = '1';
-let currScale = '1';
-
-init();
-
-async function init () {
-    const currPromises = currIds.map(currId => fetchCurr(currId));
-    const currencies = await Promise.all(currPromises);
-
-    currencies.forEach(currency => {
-        currenciesCache[currency.Curr_ID] = currency;
-    });
-
-    createCurrOptions(currencies);
-
-    document.getElementById('curr_sel').addEventListener('click', async () => {
-        let selectEl = document.getElementById('curr_sel');
-        const selectedCurrId = selectEl.value;
-        
-        if (selectedCurrId != '0') {
-            fetchedCurrRate = await getCurrRate(selectedCurrId);
-            currRate = fetchedCurrRate.Cur_OfficialRate;
-            currScale = fetchedCurrRate.Cur_Scale;
-        } else {
-            currRate = '1';
-            currScale = '1';
-        }
-        //----------------------------------------------------------------------------------------------------
-        function onClickRate() {
-            //удаление строк из таблицы, кроме первой---------------------------------------------------------
-            for(var i = 1; i < tableMain.rows.length;){
-                tableMain.deleteRow(i);
-            }
-            //добавление td в tr-------------------------------------------------------------------------------
-            for (let i = 0; i < perem.length; i++) {
-                block = document.createElement('tr');
-                tableMain.append(block);
-                for (let obj in perem[i]) {
-                    const tdList = document.createElement('td');
-                    if (obj === 'salary') {
-                        newDev = (perem[i][obj] / currRate) * currScale;  //-------------------нужный курс валют
-                        newDevCurr = newDev.toFixed(0);
-                    } else {
-                        newDevCurr = perem[i][obj];
-                    }
-                    block.append(tdList);
-                    tdList.append(newDevCurr);
-                }
-            }
-        };
-        onClickRate();
-    });
-}
-//------------------------------------------------------------------------------------------------------------
-async function fetchCurr(id) {
-    const url = id
-    ? 'https://www.nbrb.by/api/exrates/currencies/' + id
-    : 'https://www.nbrb.by/api/exrates/currencies';
-
-    const result = await fetch(url);
-    const fetchedData = await result.json();
-
-    return fetchedData;
-}
-
-function createCurrOptions(currencies) {
-    currencies.forEach(currency => {
-        const optionEl = document.createElement('option');
-        optionEl.value = currency.Cur_ID;
-        optionEl.innerText = currency.Cur_Abbreviation;
-
-        document.getElementById('curr_sel').appendChild(optionEl);
-    });
-}
-
-async function getCurrRate(currId) {
-    const response = await fetch('https://www.nbrb.by/api/exrates/rates/' + currId);
-    return response.json();
-}
-//-----------------------------------------------------------------------------------------------------
 const employeers = [
     {
         id: 0,
@@ -190,6 +107,69 @@ let qaDeptHead = {
     dept_units: [qaLead],
     all_stuff: []
 };
+//--------------------------------------------------------------------------------------------
+const currIds = [145, 292, 298];
+const currenciesCache = {};
+
+let currRate = '1';
+let currScale = '1';
+let perem = [];
+
+init();
+
+async function init () {
+    const currPromises = currIds.map(currId => fetchCurr(currId));
+    const currencies = await Promise.all(currPromises);
+
+    currencies.forEach(currency => {
+        currenciesCache[currency.Curr_ID] = currency;
+    });
+
+    createCurrOptions(currencies);
+
+    document.getElementById('curr_sel').addEventListener('change', async () => {
+        let selectEl = document.getElementById('curr_sel');
+        const selectedCurrId = selectEl.value;
+        
+        if (selectedCurrId != '0') {
+            fetchedCurrRate = await getCurrRate(selectedCurrId);
+            currRate = fetchedCurrRate.Cur_OfficialRate;
+            currScale = fetchedCurrRate.Cur_Scale;
+        } else {
+            currRate = '1';
+            currScale = '1';
+        }
+        //вызов функции buildTable---------------------------------------------------------
+        buildTable();
+    });
+};
+//------------------------------------------------------------------------------------------------------------
+async function fetchCurr(id) {
+    const url = id
+    ? 'https://www.nbrb.by/api/exrates/currencies/' + id
+    : 'https://www.nbrb.by/api/exrates/currencies';
+
+    const result = await fetch(url);
+    const fetchedData = await result.json();
+
+    return fetchedData;
+};
+
+function createCurrOptions(currencies) {
+    currencies.forEach(currency => {
+        const optionEl = document.createElement('option');
+        optionEl.value = currency.Cur_ID;
+        optionEl.innerText = currency.Cur_Abbreviation;
+
+        document.getElementById('curr_sel').appendChild(optionEl);
+    });
+};
+
+async function getCurrRate(currId) {
+    const response = await fetch('https://www.nbrb.by/api/exrates/rates/' + currId);
+    return response.json();
+};
+
 //добавление в массив сотрудников отдела---------------------------------------------------------
 developer.all_stuff = employeers.filter(developer => developer.dept_unit_id === 2);
 devLead.all_stuff = employeers.filter(devLead => devLead.dept_unit_id === 1);
@@ -197,6 +177,8 @@ devDeptHead.all_stuff = employeers.filter(devDeptHead => devDeptHead.dept_unit_i
 qaTester.all_stuff = employeers.filter(qaTester => qaTester.dept_unit_id === 5);
 qaLead.all_stuff = employeers.filter(qaLead => qaLead.dept_unit_id === 4);
 qaDeptHead.all_stuff = employeers.filter(qaDeptHead => qaDeptHead.dept_unit_id === 3);
+
+
 const left = document.getElementById('left');
 // переключение жирного--------------------------------------------------------------------------
 left.addEventListener('click', function (ev) {
@@ -246,14 +228,8 @@ left.addEventListener('click', function (ev) {
 //---------------------------------------------------------------------------------------------
 const Devel = document.getElementById('dev');
 const LeadDev = document.getElementById('leaddev');
-const DevMan = document.getElementById('devman');
 const Testers = document.getElementById('testers');
 const LeadQa = document.getElementById('leadqa');
-const QualMan = document.getElementById('qualman');
-const Spandevmam = document.getElementById('spandevmam');
-const Spanleaddev= document.getElementById('spanleaddev');
-const Spanqualman = document.getElementById('spanqualman');
-const Spanleadqa = document.getElementById('spanleadqa');
 // шеврон------------------------------------------------------------------------------------------
 left.addEventListener('click', function (ev) {
     if (ev.target.nodeName === 'SPAN' && ev.target.dataset.sign === 'spanLEADDEV') {
@@ -281,6 +257,10 @@ left.addEventListener('click', function (ev) {
         LeadQa.classList.toggle('displayNone');
     }
 });
+const Spandevmam = document.getElementById('spandevmam');
+const Spanleaddev= document.getElementById('spanleaddev');
+const Spanqualman = document.getElementById('spanqualman');
+const Spanleadqa = document.getElementById('spanleadqa');
 // кнопка очистить------------------------------------------------------------------------------------------
 document.getElementById('clear').addEventListener('click', function (ev) {
     if (ev.target.nodeName === "BUTTON") {
@@ -313,10 +293,8 @@ document.getElementById('clear').addEventListener('click', function (ev) {
         currRate = '1';
         currScale = '1';
         perem = [];
-        //----------------------------------------
-        for(var i = 1; i < tableMain.rows.length;){
-        tableMain.deleteRow(i);
-        }
+        // удаление tBody----------------------------------------------------------------------
+        deleteTBody();
     }
 })
 //проверяем какой отдел выбираем-------------------------------------------------------------------------------
@@ -345,28 +323,41 @@ left.addEventListener('click', function (ev) {
         perem = qaDeptHead.all_stuff;
         ev.target.classList.add('bold');
     }  
-    //удаление строк из таблицы, кроме первой-----------------------------------------------------------------
-    for(var i = 1; i < tableMain.rows.length;){
-        tableMain.deleteRow(i);
+    //вызов функции buildTable-----------------------------------------------------------------  
+    if (ev.target.nodeName === "LI") {
+        buildTable();
     }
-    //добавление строк tr в которых находятся колонки td---------------------------------------------------------
+    
+});
+
+// создание таблицы-----------------------------------------------------------------------------
+function buildTable () {
+    // удаление tBody---------------------------------------------------------------------------
+    deleteTBody();
+    // создание tBody---------------------------------------------------------------------------
+    var tBody = document.createElement('tbody');
     for (let i = 0; i < perem.length; i++) {
-        block = document.createElement('tr');
-        tableMain.append(block);
-        for (let obj in perem[i]) {
-            const tdList = document.createElement('td');
-            // if (currRate === undefined) {
-            //     currRate = '1';
-            //     currScale = '1';
-            // }
-            if (obj === 'salary') {
-                newDev = (perem[i][obj] / currRate * currScale);  //--------------------------------------нужный курс валют
-                newDevCurr = newDev.toFixed(0);
-            } else {
-                newDevCurr = perem[i][obj];
-            }
-            block.append(tdList);
-            tdList.append(newDevCurr);
-        }
-    }
-})
+        var trBlock = document.createElement('tr');
+            for (let obj in perem[i]) {
+                if (obj === 'salary') {
+                    newDev = (perem[i][obj] / currRate * currScale);  //----------------курс валют
+                    newDevCurr = newDev.toFixed(0);
+                } else {
+                    newDevCurr = perem[i][obj];
+                }
+                var tdList = document.createElement('td');
+                tdList.append(newDevCurr); // вставляем данные в td
+                trBlock.append(tdList); // td вставляем в tr
+                tBody.append(trBlock); // tr вставляем в элемент tbody
+                tableMain.append(tBody); // tbody вставляем в таблицу
+            };
+    };
+};
+
+// удаление tBody-------------------------------------------------------------------------------
+function deleteTBody() {
+    var tBody = document.getElementsByTagName('tbody');
+    for (let i = 0; tBody.length > i; i++) {
+        tBody[i].remove();
+    };
+};
